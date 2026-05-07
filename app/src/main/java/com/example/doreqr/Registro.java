bb14a6ca93a797b1fbeee3f59a34d462e82223ca
Juan Pablo Cielo <juanpabloct4@gmail.com>
2026-05-07 15:47:24 -0600

    Se agregó la navegacion Login-Registro y se agregaron iconos al formulario

--- app/src/main/java/com/example/doreqr/Registro.java
+++ app/src/main/java/com/example/doreqr/Registro.java
@@ -1,6 +1,9 @@
 package com.example.doreqr;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Registro extends AppCompatActivity {
+
+    Button btnRegistarRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,15 @@ public class Registro extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnRegistarRegistro = findViewById(R.id.btn1);
+        btnRegistarRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivityForResult(intent, 0);
+            }
         });
     }
 }
